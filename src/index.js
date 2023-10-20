777a8ddf7f2154b90203b33ae475ef8502975e7b
Penguwyn <147259905+Penguwyn@users.noreply.github.com>
2023-10-20 15:46:26 +0200

    Next1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
+import { useState } from "react";
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./index.css";
+
+function App() {
+  const [dogImg, setDogImg] = useState("");
+  const [buttonState, setButtonState] = useState(false);
+  const [showPic, setShowPic] = useState(false);
+  
+
+  const toggleImage = () => {
+	setButtonState(true);
+	  fetch('https://dog.ceo/api/breeds/image/random', { method: 'GET' })
+	  .then((response) => response.json()) 
+	  .then((commits) => {
+      setDogImg(commits.message);
+	  setButtonState(false);
+	  setShowPic(true);
+		  })
+  };
+  
+
+  return (
+    <div>
+      <h1>React Image Display</h1>
+	  <p>
+		<button onClick={toggleImage} id="DogDisplay" disabled={buttonState}>Show Image</button>
+	  </p>
+	  <div className="img-container">
+	<p>
+	  {(showPic && <img src={dogImg} alt="External Image" id="PicDog" />)}
+	</p>
+	</div>
+	</div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<App />);
+export default App;
